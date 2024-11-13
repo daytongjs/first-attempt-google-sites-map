ceab6b74118817301814d15216de5887e48de60c
daytongjs <43423169+daytongjs@users.noreply.github.com>
2024-11-13 17:05:14 -0500

    Initial commit

---
+++ layers/cities_8.js
@@ -0,0 +1,361 @@
+var json_cities_8 = {
+    "type": "FeatureCollection",
+    "name": "cities_8",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "1",
+                "City": "Cincinnati, United States",
+                "Latitude": 39.10145,
+                "Longitude": -84.51246,
+                "imageUrls": ["images/Cincinnati/image_1.JPG","images/Cincinnati/image_2.JPG","images/Cincinnati/image_3.JPG","images/Cincinnati/image_4.JPG","images/Cincinnati/image_5.JPG","images/Cincinnati/image_6.JPG"]
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-84.51246, 39.101454]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "2",
+                "City": "Washington, D.C.",
+                "Latitude": 38.89504,
+                "Longitude": -77.03654
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-77.036543, 38.895037]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "3",
+                "City": "Dublin, Ireland",
+                "Latitude": 53.34938,
+                "Longitude": -6.26056
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-6.260559, 53.349379]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "4",
+                "City": "Holyhead, UK",
+                "Latitude": 53.31067,
+                "Longitude": -4.6331
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-4.633097, 53.310671]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "5",
+                "City": "Caernarfon, UK",
+                "Latitude": 53.13944,
+                "Longitude": -4.27698
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-4.276975, 53.139436]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "6",
+                "City": "Bangor, UK",
+                "Latitude": 53.22772,
+                "Longitude": -4.12688
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-4.126882, 53.227716]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "7",
+                "City": "Gwynedd, UK",
+                "Latitude": 52.90896,
+                "Longitude": -3.83352
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-3.833524, 52.908958]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "8",
+                "City": "Flint, UK",
+                "Latitude": 53.24928,
+                "Longitude": -3.13453
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-3.134526, 53.249283]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "9",
+                "City": "Chester, UK",
+                "Latitude": 53.19089,
+                "Longitude": -2.8909
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.890896, 53.190887]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "10",
+                "City": "Liverpool, UK",
+                "Latitude": 53.4072,
+                "Longitude": -2.99168
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.99168, 53.407199]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "11",
+                "City": "Shrewsbury, UK",
+                "Latitude": 52.70776,
+                "Longitude": -2.75407
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.754066, 52.707755]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "12",
+                "City": "Newport, UK",
+                "Latitude": 51.58823,
+                "Longitude": -2.9975
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.997497, 51.588233]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "13",
+                "City": "Bath, UK",
+                "Latitude": 51.38139,
+                "Longitude": -2.3597
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.359696, 51.381386]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "14",
+                "City": "London, UK",
+                "Latitude": 51.48933,
+                "Longitude": -0.14405
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.144055, 51.489334]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "15",
+                "City": "York, UK",
+                "Latitude": 53.96566,
+                "Longitude": -1.07431
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-1.074305, 53.965658]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "16",
+                "City": "Preston, UK",
+                "Latitude": 53.75934,
+                "Longitude": -2.69927
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.699272, 53.759336]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "18",
+                "City": "Oxenholme, UK",
+                "Latitude": 54.30506,
+                "Longitude": -2.72178
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.721777, 54.30506]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "19",
+                "City": "Windermere, UK",
+                "Latitude": 54.37948,
+                "Longitude": -2.9062
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.906199, 54.379483]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "20",
+                "City": "Ambleside, UK",
+                "Latitude": 54.43195,
+                "Longitude": -2.96305
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.963053, 54.431954]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "21",
+                "City": "Bowness Pier, UK",
+                "Latitude": 54.36268,
+                "Longitude": -2.92266
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-2.922656, 54.36268]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "22",
+                "City": "Edinburgh, UK",
+                "Latitude": 55.95335,
+                "Longitude": -3.18838
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-3.188375, 55.953346]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "23",
+                "City": "Venice, Italy",
+                "Latitude": 45.43719,
+                "Longitude": 12.33459
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [12.33459, 45.437191]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "24",
+                "City": "Florence, Italy",
+                "Latitude": 43.7698,
+                "Longitude": 11.25564
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [11.25564, 43.769796]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "25",
+                "City": "La Spezia / Cinque Terre, Italy",
+                "Latitude": 44.10897,
+                "Longitude": 9.81578
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [9.815781, 44.108967]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "26",
+                "City": "Naples, Italy",
+                "Latitude": 40.83588,
+                "Longitude": 14.24877
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [14.248768, 40.835885]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "27",
+                "City": "Rome, Italy",
+                "Latitude": 41.89332,
+                "Longitude": 12.48293
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [12.482932, 41.89332]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": "28",
+                "City": "New York City, United States",
+                "Latitude": 40.68949,
+                "Longitude": -74.17454
+            },
+            "geometry": null
+        }
+    ]
+};
